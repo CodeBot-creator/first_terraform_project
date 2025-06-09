ab13cd3fa89d2f6fecec5f53ef8da51cb5724775
CodeBot-creator <gamlath18@gmail.com>
2025-06-10 00:55:27 +0530

    Modified Remote_state

---
+++ aws/Remote_state/setup-backend.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "your-unique-s3-bucket-name" # must be globally unique
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
